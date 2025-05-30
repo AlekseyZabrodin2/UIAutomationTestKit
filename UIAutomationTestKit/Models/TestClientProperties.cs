52f807f6775bac04e1d6e21f05d9556d0bf11425
ADANI\zabrodin <rediskal50@gmail.com>
2025-05-30 11:33:46 +0300

    ref: create config and properties for config file

---
+++ UIAutomationTestKit/Models/TestClientProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIAutomationTestKit.Models
+{
+    public class TestClientProperties
+    {
+        public string TestClientPath { get; set; }
+        public string TestClientDir { get; set; }
+        public bool EnableRowVirtualization { get; set; }
+    }
+}
---
+++ UiAutoTests/Clients/TestClientProperties.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
+
+namespace UiAutoTests.Clients
+{
+    public class TestClientProperties
+    {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+
+
+        public string TestClientPath { get; set; }
+        public string TestClientDir { get; set; }
+        public bool EnableRowVirtualization { get; set; }
+
+
+        public string ChooseConfig(bool configWithTrue)
+        {
+            return configWithTrue
+                ? Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Clients") 
+                : Path.Combine(AppContext.BaseDirectory, "Clients");
+        }
+
+        public void LoadConfig(string path)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(path)
+                .AddJsonFile("testClientProperties.json")
+                .Build();
+
+            var section = configuration.GetSection(nameof(TestClientProperties));
+            var result = new TestClientProperties();
+            section.Bind(result);
+
+            _logger.Info($"Enable Row Virtualization - [{result.EnableRowVirtualization}]");
+        }
+    }
+}
