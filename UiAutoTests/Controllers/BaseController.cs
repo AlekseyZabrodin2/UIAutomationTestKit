1b2c924fd097d5d51b55fa0e43fe1c326dc968b2
Aleksey Zabrodin <alekseyzabrodin2.0@gmail.com>
2025-09-08 09:51:09 +0300

    ref (feat): add base controller

---
+++ UiAutoTests/Controllers/BaseController.cs
@@ -0,0 +1,72 @@
+﻿using NLog;
+using System.ComponentModel;
+using UiAutoTests.Core;
+using UiAutoTests.Helpers;
+using UiAutoTests.Services;
+
+namespace UiAutoTests.Controllers
+{
+    public class BaseController
+    {
+        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+        private LoggerHelper _loggerHelper = new();
+        public HtmlReportService _reportService = new();
+
+
+
+        public void ExecuteTest(ITestClient testClient, string testName, Action testAction)
+        {
+            _loggerHelper.LogEnteringTheMethod();
+
+            try
+            {
+                testAction.Invoke();
+
+                _loggerHelper.LogCompletedResult(testName, _reportService);
+            }
+            catch (Exception exception)
+            {
+                _loggerHelper.LogFailedResult(testName, exception, _reportService);
+                throw;
+            }
+            finally
+            {
+                EnsureClientStopped(testClient);
+            }
+        }
+
+        public void EnsureClientStopped(ITestClient testClient, string clientName = "default")
+        {
+            _loggerHelper.LogEnteringTheMethod();
+
+            try
+            {
+                if (testClient == null)
+                {
+                    _logger.Debug($"Client '{clientName}' is null - nothing to stop");
+                    return;
+                }
+
+                testClient.Kill();
+                _logger.Debug($"Client '{clientName}' stopped successfully");
+            }
+            catch (ObjectDisposedException)
+            {
+                _logger.Debug($"Client '{clientName}' already disposed");
+            }
+            catch (InvalidOperationException ex)
+            {
+                _logger.Debug(ex, $"Client '{clientName}' already stopped or in invalid state");
+            }
+            catch (Win32Exception ex)
+            {
+                _logger.Warn(ex, $"Win32 exception during client '{clientName}' shutdown");
+            }
+            catch (Exception ex)
+            {
+                _logger.Warn(ex, $"Failed to stop client '{clientName}' safely");
+            }
+        }
+
+    }
+}
