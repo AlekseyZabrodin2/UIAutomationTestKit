51869465311b3aa56da9a1602b0e94d59465d2b2
ADANI\zabrodin <rediskal50@gmail.com>
2025-09-12 12:40:29 +0300

    ref (test): add test to finding menu and menu items

--- UiAutoTests/Tests/UIAutomationTests/TestsForMenu.cs
+++ UiAutoTests/Tests/UIAutomationTests/TestsForMenu.cs
@@ -21,19 +21,53 @@ namespace UiAutoTests.Tests.UIAutomationTests
         
 
         [Test]
-        public void Test01_Can_Navigate_To_CSharp_Course()
+        public void Test01_CanNavigateToCSharpCourse()
         {
             _mainWindowController = GetController<MainWindowController>();
             _mainWindowController.ExecuteTest(_testClient, _testName, () =>
             {
                 _mainWindowController
-                        .ExpandMenuItemById("CoursesMenuItem")
-                        .ExpandMenuItemById("ProgrammingMenuItem")
-                        .ClickMenuItemById("C#MenuItem");
-
-                Thread.Sleep(3000);
+                    .ExpandMenuItemById("CoursesMenuItem")
+                    .ExpandMenuItemById("ProgrammingMenuItem")
+                    .ClickMenuItemById("C#MenuItem")
+                    .Pause(3000);
             });
         }
+
+        [Test]
+        public void Test02_ShouldContainMainItems()
+        {
+            _mainWindowController = GetController<MainWindowController>();
+            _mainWindowController.ExecuteTest(_testClient, _testName, () =>
+            {
+                _mainWindowController
+                    .AssertMainMenuItemsCountIs(7)
+                    .Pause(3000);
+            });
+        }
+
+        [Test]
+        public void Test03_ShouldContainSubitemsCount()
+        {
+            _mainWindowController = GetController<MainWindowController>();
+            _mainWindowController.ExecuteTest(_testClient, _testName, () =>
+            {
+                var menuItem = "CoursesMenuItem";
+                var subItem = "DataScienceMenuItem";
+
+                _mainWindowController
+                    .ExpandMenuItemById(menuItem)
+                    .ExpandMenuItemById(subItem)
+                    .Pause(1500)
+                    .AssertSubItemsCountIs(subItem, 3);
+            });
+        }
+
+
+
+
+
+
 
     }
 }
