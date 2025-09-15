87da2e4fef5d6df04dff483960ff66013ca1e260
ADANI\zabrodin <rediskal50@gmail.com>
2025-09-15 15:14:38 +0300

    feat: create new window

---
+++ UIAutomationTestKit/Views/AboutAppView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UIAutomationTestKit.Views
+{
+    /// <summary>
+    /// Interaction logic for AboutAppView.xaml
+    /// </summary>
+    public partial class AboutAppView : Window
+    {
+        public AboutAppView()
+        {
+            InitializeComponent();
+        }
+    }
+}
