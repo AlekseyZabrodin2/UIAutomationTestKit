7b379828438c41e998a33ea84cc0b19c36b478f2
ADANI\zabrodin <rediskal50@gmail.com>
2025-09-15 15:15:36 +0300

    feat: create class with locators for about app window

---
+++ UiAutoTests/Locators/AboutAppWindowLocators.cs
@@ -0,0 +1,28 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Exceptions;
+
+namespace UiAutoTests.Locators
+{
+    internal class AboutAppWindowLocators
+    {
+        private readonly Window _aboutWindow;
+        private readonly ConditionFactory _conditionFactory;
+
+
+        public AboutAppWindowLocators(Window window, ConditionFactory conditionFactory)
+        {
+            _aboutWindow = window;
+            _conditionFactory = conditionFactory;
+        }
+
+        private AutomationElement FindFirstById(string automationId) =>
+             _aboutWindow.FindFirstDescendant(_conditionFactory.ByAutomationId(automationId))
+             ?? throw new ElementNotAvailableException($"Element with AutomationId - [{automationId}] not found");
+
+
+        public Window AboutAppWindow => FindFirstById("AboutAppView").AsWindow();
+        public Button AboutAppOkButton => FindFirstById("AboutAppOkButton").AsButton();
+
+    }
+}
